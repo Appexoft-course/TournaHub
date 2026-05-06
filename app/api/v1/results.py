27efc2828e1e14bbf8459f401c304b1d9070f0c4
Єлизавета Галабурда <elizavetagalaburda@MacBook-Air-Elizaveta.local>
2026-05-06 19:17:25 +0300

    add websocket support and statistics tests

--- app/api/v1/results.py
+++ app/api/v1/results.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
+from app.core.websocket_manager import manager
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db
@@ -11,13 +12,37 @@ router = APIRouter()
 @router.patch("/matches/{match_id}", response_model=MatchResultResponse)
 async def update_result(
     match_id: int,
-    data: MatchResultUpdate,
+    result_data: MatchResultUpdate,
     db: AsyncSession = Depends(get_db),
 ):
-    return await update_match_result(
+    updated_match = await update_match_result(
         db=db,
         match_id=match_id,
-        score_1=data.score_1,
-        score_2=data.score_2,
-        mvp_id=data.mvp_id,
+        score_1=result_data.score_1,
+        score_2=result_data.score_2,
+        mvp_id=result_data.mvp_id,
     )
+
+    await manager.broadcast(
+        {
+            "type": "match_result_updated",
+            "match_id": updated_match.id,
+            "score_1": updated_match.score_1,
+            "score_2": updated_match.score_2,
+            "winner": updated_match.winner,
+            "loser": updated_match.loser,
+            "mvp_id": updated_match.mvp_id,
+        }
+    )
+
+    return updated_match
+
+@router.websocket("/ws/matches/results")
+async def match_results_websocket(websocket: WebSocket):
+    await manager.connect(websocket)
+
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.v1.statistics import router as statistics_router
+from app.api.v1.results import router as results_router
 from app.api.v1.routers.auth import router as auth_router
 from app.api.v1.routers.tournament import router as tor_router
+
 api_router = APIRouter()
 
 api_router.include_router(
@@ -12,11 +14,19 @@ api_router.include_router(
 )
 
 api_router.include_router(
+    results_router,
+    prefix="/results",
+    tags=["results"],
+)
+
+api_router.include_router(
     auth_router,
     prefix="/auth",
     tags=["Auth"],
 )
 
-api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-
-api_router.include_router(tor_router, prefix="/tournament", tags=["Tour"])
+api_router.include_router(
+    tor_router,
+    prefix="/tournament",
+    tags=["Tour"],
+)
